dd94f689475de9f34a0be120a1e5a079a8ed8e70
Anastas Metaxa <metaxaanastas@mail.ru>
2021-10-28 13:57:34 +0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         int currentBalance = 100;     //Текущий баланс счета в рублях
         int transferAmount = 1_100;   //Сумма пополнения счета в рублях
 
-        boolean bonusActivation = transferAmount >= 1000;                           //Условие активации бонуса
-        int bonus = bonusActivation ? (currentBalance + transferAmount) / 100 : 0;  //Расчет бонуса в рублях
-        int totalBalance = currentBalance + transferAmount + bonus;                 //Расчет итогового баланса счета в рублях
+        boolean bonusActivation = transferAmount >= 1000;              //Условие активации бонуса
+        int bonus = bonusActivation ? transferAmount / 100 : 0;        //Расчет бонуса в рублях
+        int totalBalance = currentBalance + transferAmount + bonus;    //Расчет итогового баланса счета в рублях
         System.out.println("Итоговый баланс равен " + totalBalance + "руб, бонус составляет " + bonus + "руб");
     }
 }
